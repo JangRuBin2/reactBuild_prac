6f944dcb0193eee6069edcd6f3ab9325582961eb
jangrubin <wkdfnqls2465@gmail.com>
2023-06-02 17:36:01 +0900

    빌드에 사용할 폴더 생성, index.html 빌드 폴더로 이동

--- server/app.ts
+++ server/app.ts
@@ -1,13 +1,15 @@
 import express from "express";
 import path from "path";
 const root = path.join(__dirname, '../');
-console.log(root)
+// console.log(root)
 const app = express();
 // 루트 경로 미들웨어
 app.use(express.static(path.join(root)));
+// 최초 접속 요청 처리
 app.get('/', (req, res)=> {
   res.sendFile('index.html', {root : root});
 })
+// 서버 실행
 app.listen(8080, ()=> {
   console.log("서버 실행")
 })
