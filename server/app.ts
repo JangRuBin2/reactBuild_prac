5a8704a32c04b1319a97b3b44da20c3c6cd95eb1
jangrubin <wkdfnqls2465@gmail.com>
2023-06-02 17:32:02 +0900

    서버(app.ts) 위치 이동, root 절대 경로 지정

---
+++ server/app.ts
@@ -0,0 +1,6 @@
+import express from "express";
+import path from "path";
+const root = path.join(__dirname, '../');
+console.log(root)
+const app = express();
+// app.use(express.static(__dirname));
