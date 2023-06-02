0d147fd11ffe6e3efdd51efd6bdd285e119acf1f
jangrubin <wkdfnqls2465@gmail.com>
2023-06-02 17:34:17 +0900

    서버 실행 및 root/index.html 파일 정상 출력 확인

--- server/app.ts
+++ server/app.ts
@@ -3,4 +3,11 @@ import path from "path";
 const root = path.join(__dirname, '../');
 console.log(root)
 const app = express();
-// app.use(express.static(__dirname));
+// 루트 경로 미들웨어
+app.use(express.static(path.join(root)));
+app.get('/', (req, res)=> {
+  res.sendFile('index.html', {root : root});
+})
+app.listen(8080, ()=> {
+  console.log("서버 실행")
+})
